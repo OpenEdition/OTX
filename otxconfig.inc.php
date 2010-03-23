0e781c2b0a1a23e62185ee0416e8c0a5ec5a6c8e
barts <nicolas.barts@revues.org>
2010-03-23 16:49:09 +0100

    Hello world

---
+++ otxconfig.inc.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * servoo2.inc.php
+ * PHP >= 5.2
+ * @author Nicolas Barts
+ * @copyright 2010, CLEO/Revues.org
+ * @licence http://www.gnu.org/copyleft/gpl.html
+**/
+
+
+/** OTX URI Location **/
+define('__OTX_URI__',    "http://ccsdrv10.in2p3.fr/otx/");
+
+/** OTX ROOT PATH Location **/
+define('__OTX_PWD__',    "/data/www/otx/");
+
+?>
