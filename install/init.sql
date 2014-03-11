11627a9bbde5e9b44c8850f9fc367a9284e326d8
administrateur <administrateur@revues.org>
2014-03-11 16:19:02 +0100

    Mise à jour du fichier d'install sql

--- install/init.sql
+++ install/init.sql
@@ -1,31 +1,92 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE Document (
-  idDocument integer PRIMARY KEY,
-  pathDocument varchar(255) NOT NULL default '',
-  nbNotes integer NOT NULL default '0',
-  realname varchar(255) NOT NULL default '',
-  tmpname varchar(255) NOT NULL default ''
-);
-CREATE TABLE NoteTexte (
-  numNote integer NOT NULL default '0',
-  texteNote text NOT NULL,
-  idDocument integer NOT NULL default '0',
-  PRIMARY KEY  (numNote,idDocument)
-);
-CREATE TABLE users (
-  id integer PRIMARY KEY,
-  username varchar(255) NOT NULL default '',
-  password varchar(255) NOT NULL default ''
-);
-CREATE TABLE NotePossible (
-  idNote integer AUTO_INCREMENT,
-  numNote integer NOT NULL default '0',
-  motNote varchar(255) NOT NULL default '',
-  extraitMotNote text NOT NULL,
-  probaNote tinyint(4) NOT NULL default '0',
-  idDocument integer NOT NULL default '0',
-  PRIMARY KEY  (idNote,idDocument)
-);
-COMMIT;
+-- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: opentext_devel
+-- ------------------------------------------------------
+-- Server version	5.5.35-0+wheezy1-log
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Document`
+--
+
+DROP TABLE IF EXISTS `Document`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Document` (
+  `idDocument` int(11) NOT NULL AUTO_INCREMENT,
+  `pathDocument` varchar(255) NOT NULL DEFAULT '',
+  `nbNotes` int(11) NOT NULL DEFAULT '0',
+  `realname` varchar(255) NOT NULL DEFAULT '',
+  `tmpname` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`idDocument`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `NotePossible`
+--
+
+DROP TABLE IF EXISTS `NotePossible`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `NotePossible` (
+  `idNote` int(11) NOT NULL AUTO_INCREMENT,
+  `numNote` int(11) NOT NULL DEFAULT '0',
+  `motNote` varchar(255) NOT NULL DEFAULT '',
+  `extraitMotNote` text NOT NULL,
+  `probaNote` tinyint(4) NOT NULL DEFAULT '0',
+  `idDocument` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idNote`,`idDocument`)
+) ENGINE=InnoDB AUTO_INCREMENT=982 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `NoteTexte`
+--
+
+DROP TABLE IF EXISTS `NoteTexte`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `NoteTexte` (
+  `numNote` int(11) NOT NULL DEFAULT '0',
+  `texteNote` text NOT NULL,
+  `idDocument` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`numNote`,`idDocument`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-03-11 16:18:40
