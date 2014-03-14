a9784f331c247a5e1ece8fef8236304b7f638b15
Nahuel Angelinetti <nahuel.angelinetti@openedition.org>
2014-03-14 11:15:37 +0100

    Correction du script de création d'utilisateur avec PDO

--- install/create_user.php
+++ install/create_user.php
@@ -5,21 +5,16 @@ if (php_sapi_name() !== 'cli') die("Execution not allowed");
 if(sizeof($argv) !== 3) die("usage : php install/create_user.php [username] [password]\n");
 
 ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . 'server' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR );
-require_once('adodb/adodb.inc.php');
 require_once('OTXConfig.class.php');
 
 $username = $argv[1];
 $password = $argv[2];
 
 $config = OTXConfig::singleton();
-$db = ADONewConnection("pdo");
-
-$dbh = new PDO($config->db);
-
-$db->connect($config->db);
+$db = new PDO($config->db['dsn'], $config->db['user'], $config->db['password']);
 
 $password_crypted = crypt($password);
 
-$db->execute('INSERT INTO users (username, password) VALUES(' . $db->quote($username) . ', ' . $db->quote($password_crypted) . ')');
+$db->query('INSERT INTO users (username, password) VALUES(' . $db->quote($username) . ', ' . $db->quote($password_crypted) . ')');
 
 die("User created successfully\n");
