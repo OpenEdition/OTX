7e8e86b4c7b9ce2a45d2f1af1ac7cb180ec91c3f
ArnAud <arnaud.cordier@openedition.org>
2015-07-09 11:12:12 +0200

    orphannotes: -- pour les options longues de soffice

--- plugins/orphannotes/orphannotes.class.php
+++ plugins/orphannotes/orphannotes.class.php
@@ -115,13 +115,12 @@ class orphannotes extends Plugin {
 		$temp_profile = sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid('OTX');
 		mkdir($temp_profile, 0755, true);
 
-		$command = "{$this->_config->soffice['officepath']} --norestore --headless -env:UserInstallation=file://{$temp_profile} --convert-to {$convertTo} -outdir {$out} {$in}";
+		$command = "{$this->_config->soffice['officepath']} --norestore --headless -env:UserInstallation=file://{$temp_profile} --convert-to {$convertTo} --outdir {$out} {$in}";
 
 		$returnvar = 0;
 		$result    = '';
 
 		$output = exec($command, $result, $returnvar);
-
 		/* Suppression du profile temporaire */
 		$this->rmdir($temp_profile);
 
